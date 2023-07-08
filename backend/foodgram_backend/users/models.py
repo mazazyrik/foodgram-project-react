952db17b06c476b07959ca5a7569ab6471eb2f81
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2023-07-08 17:57:56 +0300

    user model added, api started

--- backend/foodgram_backend/users/models.py
+++ backend/foodgram_backend/users/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'username', 'first_name', 'last_name'
+    ]
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name='email'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='first name'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='last name'
+    )
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        ordering = ('pk', )
+
+    @property
+    def is_moderator(self):
+        return self.is_staff
+
+    @property
+    def is_admin(self):
+        return self.is_superuser
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='follower',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='following',
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['follower', 'author', ],
+                name='Unique follow'
+            )
+        ]
+        verbose_name = 'follow'
+        verbose_name_plural = 'follow'
