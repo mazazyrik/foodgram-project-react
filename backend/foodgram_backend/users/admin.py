47c15c6c6619ee3bcf1a9e09e161d1d37f679a26
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2023-07-08 19:26:05 +0300

    user model added, api started

--- backend/foodgram_backend/users/admin.py
+++ backend/foodgram_backend/users/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Follow
 
 
 class UserAdmin(admin.ModelAdmin):
     list_filter = ('username', 'email')
 
 
+class FollowAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Follow, FollowAdmin)
